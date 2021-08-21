178ea5f8f9c2ab5d2fd67e70262653f42095ae2b
Nav <navjot.singh@pepcoding.com>
2021-08-20 23:53:41 -0700

    updated Recommendation Card

--- src/components/RecommendationCard/RecommendationCard.js
+++ src/components/RecommendationCard/RecommendationCard.js
@@ -1,4 +1,12 @@
-import { Avatar, Button, Typography } from "@material-ui/core";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  CardActions,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import styles from "./RecommendationCard.module.css";
 import { IconButton } from "@material-ui/core";
@@ -8,27 +16,34 @@ const RecommendationCard = (props) => {
   const data = props.data;
   console.log(data);
   return (
-    <div className={styles.recommend__card}>
-      <div className={styles.userProfilePhoto}>
-        <Avatar src={data.userPhoto} />
-      </div>
-      <div className={styles.recommend}>
-        <div className={styles.recommender__info}>
-          <div className={styles.username}>{data.username}</div>
-          <div className={styles.userPosition}>{data.userPosition}</div>
-        </div>
-        <div className={styles.recommended__content}>
-          {data.recommendComment}
-        </div>
-
-        <div className={styles.watchVideo__btn}>
-          <div className={styles.video__log}>
-            <img src={VideoIcon} />
+    <>
+      <Card className={styles.recommend__card}>
+        <CardContent style={{ display: "flex", padding:'1rem 0.7rem 0 0.7rem' }}>
+          <div className={styles.userProfilePhoto}>
+            <Avatar src={data.userPhoto} />
           </div>
-          <div className={styles.watchBtn__text}>Watch Video</div>
-        </div>
-      </div>
-    </div>
+          <div className={styles.card__content}>
+            <div className={styles.recommend}>
+            <div className={styles.recommender__info}>
+              <div className={styles.username}>{data.username}</div>
+              <div className={styles.userPosition}>{data.userPosition}</div>
+            </div>
+            <div className={styles.recommended__content}>
+              {data.recommendComment}
+            </div>
+            </div>
+            <CardActions>
+              <div className={styles.watchVideo__btn}>
+                <div className={styles.video__log}>
+                  <img src={VideoIcon} />
+                </div>
+                <div className={styles.watchBtn__text}>Watch Video</div>
+              </div>
+            </CardActions>
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
